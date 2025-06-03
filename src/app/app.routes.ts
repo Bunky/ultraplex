c1a645480451cc163f33134e39a6afc9fc9d4fdd
Bunky <ben.adam.charles@gmail.com>
2025-06-03 09:21:36 +0100

    Basic routing setup
    Also fixed couple of naming conventions from v20.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./pages/dashboard/dashboard.component').then((m) => m.DashboardComponent)
+  },
+  {
+    path: 'cinemas',
+    loadComponent: () => import('./pages/cinemas/cinemas.component').then((m) => m.CinemasComponent)
+  },
+  {
+    path: 'movies',
+    loadComponent: () => import('./pages/movies/movies.component').then((m) => m.MoviesComponent)
+  },
+  {
+    path: '**',
+    component: AppComponent // TODO: Handle 404 with a NotFoundComponent
+  }
+];

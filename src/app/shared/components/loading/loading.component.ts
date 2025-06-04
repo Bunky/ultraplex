e585a1bb68206b7b0f68a103aaf66f693407c0ab
Bunky <ben.adam.charles@gmail.com>
2025-06-04 15:52:43 +0100

    Added loading component

---
+++ src/app/shared/components/loading/loading.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+@Component({
+  selector: 'app-loading',
+  imports: [MatProgressSpinnerModule],
+  templateUrl: './loading.component.html',
+  styleUrl: './loading.component.scss'
+})
+export class LoadingComponent {
+  protected readonly size = input<number>(25);
+}
